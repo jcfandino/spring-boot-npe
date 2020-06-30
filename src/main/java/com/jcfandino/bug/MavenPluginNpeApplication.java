05e68d8a2683641fadb5beec3a920ee8c5f08611
Juan Cruz Fandino <juan@gloot.com>
2020-06-30 23:53:12 +0200

    bug report

---
+++ src/main/java/com/jcfandino/bug/MavenPluginNpeApplication.java
@@ -0,0 +1,13 @@
+package com.jcfandino.bug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPluginNpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPluginNpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcfandino/bug/MavenPluginNpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcfandino.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPluginNpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
